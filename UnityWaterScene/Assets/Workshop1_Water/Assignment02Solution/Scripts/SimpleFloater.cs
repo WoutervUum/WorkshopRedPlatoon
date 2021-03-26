ab3ee506149eaf9fba74782fcdcdad14034000c4
WoutervUum <60775182+WoutervUum@users.noreply.github.com>
2021-03-26 11:25:43 +0100

    Workshops
    yes

---
+++ UnityWaterScene/Assets/Workshop1_Water/Assignment02Solution/Scripts/SimpleFloater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFloater : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+
+    //How high should the object float over the water
+    public float depthBeforeSubmerged = 0f;
+    //Increase/Decrease how fast the object should float upwards
+    public float displacementAmount = 3f;
+
+    void FixedUpdate()
+    {
+        //Get WaveHeight of object
+        float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x);
+        if (transform.position.y < waveHeight)
+        {
+            float displacementMultiplier = Mathf.Clamp01((waveHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+            rigidBody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+        }
+    }
+}
---
+++ UnityWaterScene/Assets/Workshop1_Water/Assignment03Solution/Scripts/SimpleRectangleFloater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRectangleFloater : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+    public Transform[] pressurePoints;
+    public float depthBeforeSubmerged = 1f;
+    public float displacementAmount = 3f;
+    public float waterDrag = 0.99f;
+    public float waterAngularDrag = 0.5f;
+    void FixedUpdate()
+    {
+        foreach (Transform PressurePoint in pressurePoints)
+        {
+            rigidBody.AddForceAtPosition(Physics.gravity / pressurePoints.Length, PressurePoint.transform.position, ForceMode.Acceleration);
+            float waveHeight = WaveManager.instance.GetWaveHeight(PressurePoint.transform.position.x);
+            if (PressurePoint.transform.position.y < waveHeight)
+            {
+                float displacementMultiplier = Mathf.Clamp01((waveHeight - PressurePoint.transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+                rigidBody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), PressurePoint.transform.position, ForceMode.Acceleration);
+                rigidBody.AddForce(displacementMultiplier * -rigidBody.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+                rigidBody.AddTorque(displacementMultiplier * -rigidBody.angularVelocity * waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            }
+        }
+    }
+}
